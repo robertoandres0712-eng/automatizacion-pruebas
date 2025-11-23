e96fbdbb67e880110f065fdd31d1d3512e9332f9
robertoandres0712-eng <tuemail@correo.com>
2025-11-22 21:36:22 -0300

    Actividad 1: configuración Maven con JUnit y Selenium

---
+++ src/main/java/cl/roberto/automatizacion/App.java
@@ -0,0 +1,7 @@
+package cl.roberto.automatizacion;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Proyecto de Automatización funcionando correctamente.");
+    }
+}
