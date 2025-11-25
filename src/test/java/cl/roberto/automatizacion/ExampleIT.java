2f79c4b9bbeef0159dd02457fcb374df833292ff
robertoandres0712-eng <roberto.andres0712@gmail.com>
2025-11-24 21:59:21 -0300

    Add integration test ExampleIT with a simple assertion

---
+++ src/test/java/cl/roberto/automatizacion/ExampleIT.java
@@ -0,0 +1,12 @@
+package cl.roberto.automatizacion;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleIT {
+
+    @Test
+    void pruebaIntegracion() {
+        assertTrue(true);
+    }
+}
