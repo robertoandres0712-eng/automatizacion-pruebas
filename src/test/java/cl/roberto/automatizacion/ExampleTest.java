83a2cd8b7c84de012069e49fd9154a5336747bae
robertoandres0712-eng <roberto.andres0712@gmail.com>
2025-11-24 21:58:50 -0300

    Add unit test for addition functionality

---
+++ src/test/java/cl/roberto/automatizacion/ExampleTest.java
@@ -0,0 +1,12 @@
+package cl.roberto.automatizacion;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleTest {
+
+    @Test
+    void pruebaUnitaria() {
+        assertEquals(2, 1 + 1);
+    }
+}
